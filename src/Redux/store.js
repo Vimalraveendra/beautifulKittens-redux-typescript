274b2c2ff469a7e0c8a9b3d518bac1a139169be0
vimal Raveendran <vimalraveendra@gmail.com>
2020-04-28 11:08:54 +0200

    updated the store to logout the files only in development

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,7 +3,10 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-const middlewares = [logger, thunk];
+const middlewares = [thunk];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export default store;
