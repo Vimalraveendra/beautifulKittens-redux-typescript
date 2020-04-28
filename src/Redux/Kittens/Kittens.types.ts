99c22b8587d36b8fae2b1345efedfa7fcd78015d
vimal Raveendran <vimalraveendra@gmail.com>
2020-04-28 12:47:10 +0200

    Added specific types for the kittens redux state & kittend redux actions

---
+++ src/Redux/Kittens/Kittens.types.ts
@@ -0,0 +1,41 @@
+// Describing the shape of the kittens  slice of state
+export interface IKittens {
+  id: Number;
+  name: String;
+  email: String;
+  website: String;
+  phone: Number;
+}
+
+export interface IKittensArray {
+  kittens: Array<IKittens>;
+  isPending: string;
+}
+
+export const REQUEST_KITTENS_PENDING = "REQUEST_KITTENS_PENDING ";
+export const REQUEST_KITTENS_SUCCESS = "REQUEST_KITTENS_SUCCESS ";
+export const REQUEST_KITTENS_FAILED = "REQUEST_KITTENS_FAILED ";
+
+// so we are exporting each actions in typescript format
+
+export interface kittensPendingAction {
+  type: typeof REQUEST_KITTENS_PENDING;
+}
+
+export interface kittensSuccessAction {
+  type: typeof REQUEST_KITTENS_SUCCESS;
+  payload: IKittensArray;
+}
+
+export interface kittensFailedAction {
+  type: typeof REQUEST_KITTENS_FAILED;
+  payload: string;
+}
+
+// here we making the aggregation all these typescript actions
+// for that I am using the union operator or OR  operator.
+
+export type KittensActionTypes =
+  | kittensPendingAction
+  | kittensSuccessAction
+  | kittensFailedAction;
