4da86f4f7cc6b0101e213ab43e7e4e2829e2aa0f
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-24 21:51:19 +0200

    Updates

---
+++ precache-manifest.d54acae260a1ce8b23165e32c66ed99f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2513a79781f8cbc0122b72b9ec49b2",
+    "url": "/beautifulKittens-redux-typescript/index.html"
+  },
+  {
+    "revision": "737955f39e9bf5ab9084",
+    "url": "/beautifulKittens-redux-typescript/static/css/main.d7ac86f4.chunk.css"
+  },
+  {
+    "revision": "888897c1a289bde36164",
+    "url": "/beautifulKittens-redux-typescript/static/js/2.27565145.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beautifulKittens-redux-typescript/static/js/2.27565145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737955f39e9bf5ab9084",
+    "url": "/beautifulKittens-redux-typescript/static/js/main.6a99f8d7.chunk.js"
+  },
+  {
+    "revision": "23ded0e6bc31195b7c2c",
+    "url": "/beautifulKittens-redux-typescript/static/js/runtime-main.83cc88d5.js"
+  },
+  {
+    "revision": "c40de6415faa48a861478f0ae43e21c7",
+    "url": "/beautifulKittens-redux-typescript/static/media/MinecraftTen-VGORe.c40de641.woff"
+  }
+]);
